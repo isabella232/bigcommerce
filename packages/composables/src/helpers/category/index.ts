4ca18e9e1dc1d47555d41e3a4ccc488ea9c8c38e
Wojciech Sikora <wsikora@vuestorefront.io>
2022-02-04 16:21:04 +0100

    refactor: remove old unused files

--- packages/composables/src/helpers/category/index.ts
+++
@@ -1,5 +0,0 @@
-export { buildBreadcrumbs } from './buildBreadcrumbs';
-export { flattenCategoryTree } from './flattenCategoryTree';
-export { getBreadcrumbs } from './getBreadcrumbs';
-export { getCategoryBySlug } from './getCategoryBySlug';
-export { getCategoryById } from './getCategoryById';
