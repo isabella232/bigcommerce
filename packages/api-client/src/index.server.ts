8557512b5e65ee0aa32c299e9c9a4e9d405ee957
Wojciech Sikora <35867383+WojtekTheWebDev@users.noreply.github.com>
2022-02-10 09:32:50 +0100

    fix: remove trailing slash from store url (#288)

--- packages/api-client/src/index.server.ts
+++ packages/api-client/src/index.server.ts
@@ -3,13 +3,26 @@ import { client } from './helpers/client';
 import { MiddlewareSettingsConfig, Endpoints } from './types';
 import api from './api';
 
-const onCreate = (settings: MiddlewareSettingsConfig) => ({
-  config: settings,
-  client: {
-    v2: client({ ...settings.sdkSettings, apiVersion: 'v2' }),
-    v3: client({ ...settings.sdkSettings, apiVersion: 'v3' })
-  }
-});
+const onCreate = (settings: MiddlewareSettingsConfig) => {
+  settings.sdkSettings.storeUrl = settings.sdkSettings.storeUrl.replace(
+    /\/$/,
+    ''
+  );
+
+  return {
+    config: settings,
+    client: {
+      v2: client({
+        ...settings.sdkSettings,
+        apiVersion: 'v2'
+      }),
+      v3: client({
+        ...settings.sdkSettings,
+        apiVersion: 'v3'
+      })
+    }
+  };
+};
 
 const { createApiClient } = apiClientFactory<
   MiddlewareSettingsConfig,
